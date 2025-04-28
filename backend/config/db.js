42740c61da5aca5301edc085ba5277e77fc5af1f
Mayank kshirsagar <mayank9180@gmail.com>
2025-04-28 17:07:34 +0530

    updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+  console.log(process.env.MONGO_URI);
+      const conn = await mongoose.connect(process.env.MONGO_URI).then(()=>{
+      console.log('connected');
+    }).catch((err)=>{
+      console.log(err)
+    })
 
-    console.log(`MongoDB connected: ${conn.connection.host}`.blue.bold);
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
   }
-};
+
+
 
 module.exports = connectDB;
